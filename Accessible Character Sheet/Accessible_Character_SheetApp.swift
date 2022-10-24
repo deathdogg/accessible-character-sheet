60f4c87051fdea640226238c78b14aa3b19ad129
Ricardo Herrera Reyes <proherrera22@gmail.com>
2022-10-24 15:42:25 -0700

    Initial Commit

---
+++ Accessible Character Sheet/Accessible_Character_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessible_Character_SheetApp.swift
+//  Accessible Character Sheet
+//
+//  Created by Ricardo Herrera on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Accessible_Character_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
